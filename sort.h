f0d12dd5aad8b952481130e10128d6b517462ff0
Metafrench <4942@holbertonschool.com>
2023-01-19 16:16:53 +0000

    mess

--- sort.h
+++ sort.h
@@ -1,11 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-/*STRUCTURE*/
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,18 +13,31 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-
-/*FUNCTIONS*/
+/* T1 tasks */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+int list_len(listint_t *h);
+void swap_ints(int *a, int *b);
+int lomuto_partition(int *array, size_t size, int l, int r);
+void lomuto_sort(int *array, size_t size, int l, int r);
+
+/* T3 tasks */
+void swap(int *a, int *b);
+void heapify(int *arr, int size_int, int i, size_t size);
+void heap_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void print_da_array(int *array, int start, int end);
+void copy_array(int *array, int start, int end, int *tmp_arr);
+void recur_split(int *tmp_arr, int start, int end, int *array);
+void merge_it(int *array, int start, int mid, int end, int *tmp_arr);
 
 #endif
